59ae4b6e12f3c043e8e3ace451fae16b1e3bf728
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-03-17 21:30:45 +0530

    get a story by id endpoint

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.use(express.json());
 app.use(postRoutes)
 
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(3000, () => console.log('server is listening on port 3000.'));
 
--- story_services.js
+++ story_services.js
@@ -15,5 +15,20 @@ router.get('/storyList', function (_, response, _) {
     });
 });
 
+//get a post by id
+router.get('/story',function(request,response,_){
+    let storyId=request.query.id;
+    console.log(storyId);
+    let sql= 'SELECT * FROM post WHERE id=?';
+    let values=[storyId];
+    connection.query(sql,values,(err,result)=>{
+        if(err) throw err;
+        response.json(result[0]);
+        response.end();
+
+    });
+
+});
+
 
 module.exports = router
